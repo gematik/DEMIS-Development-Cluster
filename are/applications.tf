3b8ce713fd00541ce88ef68a87e5b3d9e5243744
René Wiegmann Rollet <rene.rollet@noves.de>
2026-03-30 09:11:30 +0200

    feat/ARE-270: Implement secret handling for redis in ANPS
    - Move secret.tf to correct dir and add checksum-object to enable rollout upon changes
    - Define & forward variables for secretes to config-files (variables_credentials.tf, applications.tf, app-values.tftpl.yaml)
    - Add missing config params to app-values templates
    - Add validation service config for cluster version 5.5.0

--- are/applications.tf
+++ are/applications.tf
@@ -1,27 +1,32 @@
 module "are_services" {
-  source                        = "./applications"
-  external_chart_path           = local.chart_source_path
-  docker_registry               = var.docker_registry
-  helm_repository               = var.helm_repository
-  helm_repository_username      = var.helm_repository_username
-  helm_repository_password      = var.helm_repository_password
-  target_namespace              = var.target_namespace
-  pull_secrets                  = local.pull_secrets_credentials
-  deployment_information        = local.deployment_information
-  core_hostname                 = module.endpoints.core_hostname
-  portal_hostname               = module.endpoints.portal_hostname
-  meldung_hostname              = module.endpoints.meldung_hostname
-  cluster_gateway               = module.endpoints.istio_gateway_fullname
-  auth_hostname                 = module.endpoints.auth_hostname
-  debug_enabled                 = var.debug_enabled
-  istio_enabled                 = var.istio_enabled
-  context_path                  = var.context_path
-  feature_flags                 = module.application_flags.service_feature_flags
-  config_options                = module.application_flags.service_config_options
-  resource_definitions          = module.application_resources.service_resource_definitions
-  istio_proxy_default_resources = module.application_resources.istio_proxy_default_resources
-  timeout_retry_overrides       = var.timeout_retry_overrides
-  reset_values                  = var.reset_values
+  source                            = "./applications"
+  external_chart_path               = local.chart_source_path
+  docker_registry                   = var.docker_registry
+  helm_repository                   = var.helm_repository
+  helm_repository_username          = var.helm_repository_username
+  helm_repository_password          = var.helm_repository_password
+  target_namespace                  = var.target_namespace
+  pull_secrets                      = local.pull_secrets_credentials
+  deployment_information            = local.deployment_information
+  core_hostname                     = module.endpoints.core_hostname
+  portal_hostname                   = module.endpoints.portal_hostname
+  meldung_hostname                  = module.endpoints.meldung_hostname
+  cluster_gateway                   = module.endpoints.istio_gateway_fullname
+  auth_hostname                     = module.endpoints.auth_hostname
+  debug_enabled                     = var.debug_enabled
+  istio_enabled                     = var.istio_enabled
+  context_path                      = var.context_path
+  feature_flags                     = module.application_flags.service_feature_flags
+  config_options                    = module.application_flags.service_config_options
+  resource_definitions              = module.application_resources.service_resource_definitions
+  istio_proxy_default_resources     = module.application_resources.istio_proxy_default_resources
+  timeout_retry_overrides           = var.timeout_retry_overrides
+  reset_values                      = var.reset_values
+  profile_provisioning_mode_vs_are  = var.profile_provisioning_mode_vs_are
+
+  # Secrets and Credentials needed for the applications
+  redis_cus_reader_user        = var.redis_cus_reader_user
+  redis_cus_reader_password    = var.redis_cus_reader_password
 
   depends_on = [module.persistent_volume_claims, module.pull_secrets, module.activate_maintenance_mode.set_maintenance_mode]
 }
