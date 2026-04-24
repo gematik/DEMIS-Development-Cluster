8cb0da8ba77daa3832bcd59266663ff3b949249d
Noves-Developers <github@noves.de>
2026-04-24 12:21:12 +0200

    ARE-270: Implement secret handling for redis in ANPS
    - Move secret.tf to correct dir and add checksum-object to enable
    rollout upon changes
    - Define & forward variables for secretes to config-files
    (variables_credentials.tf, applications.tf, app-values.tftpl.yaml)
    - Add missing config params to app-values templates
    - Implement delegation to validation-service-are from a virtual
    validation-service
    
    ---------
    
    Co-authored-by: René Wiegmann Rollet <rene.rollet@noves.de>

--- are/variables_credentials.tf
+++ are/variables_credentials.tf
@@ -60,3 +60,15 @@ variable "docker_pull_secrets" {
 # ARE Application Credentials
 ################################
 
+# Redis CUS Credentials for ARE-Notification-Processing-Service
+variable "redis_cus_reader_user" {
+  type        = string
+  sensitive   = true
+  description = "The Redis CUS User (Reader)"
+}
+
+variable "redis_cus_reader_password" {
+  type        = string
+  sensitive   = true
+  description = "The Redis CUS Password (Reader)"
+}
